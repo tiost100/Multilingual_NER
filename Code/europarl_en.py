16c6e809dae2cebaf0f96ae4aa1d752cf5f07a9e
Tim Ostrolucký <115794574+tiost100@users.noreply.github.com>
2023-09-08 16:36:13 +0200

    Update europarl_en.py

--- Code/europarl_en.py
+++ Code/europarl_en.py
@@ -25,8 +25,6 @@ duration_spacy = stop_spacy - start_spacy
 accuracy_spacy, differences_spacy = eval_europarl(words, labels, entities_spacy, "spacy")
 
 # Print the SpaCy results
-# note: the file path depends on the storage location of the file on the 
-# computer, and can vary from computer to computer
 with open(f"{parent_dir_path}/Evaluation Results/europarl_en_spacy_eval.txt", "w") as outfile:
     outfile.write(f"Duration of the SpaCy NER in seconds: {round(duration_spacy, 3)} sec\n")
     outfile.write("\n")
@@ -52,8 +50,6 @@ duration_stanza = stop_stanza - start_stanza
 accuracy_stanza, differences_stanza = eval_europarl(words, labels, entities_stanza, "stanza")
 
 # Print the Stanza results
-# note: the file path depends on the storage location of the file on the 
-# computer, and can vary from computer to computer
 with open(f"{parent_dir_path}/Evaluation Results/europarl_en_stanza_eval.txt", "w") as outfile:
     outfile.write(f"Duration of the Stanza NER in seconds: {round(duration_stanza, 3)} sec\n")
     outfile.write("\n")
